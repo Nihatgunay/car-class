b2e5e7a36295383a73458f623a215ba5142dd338
NIhat <135535115+Nihatgunay@users.noreply.github.com>
2024-05-22 00:40:11 +0400

    Add files via upload

---
+++ Car class.cs
@@ -0,0 +1,32 @@
+﻿namespace resize___class
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+        
+        public void Drive(double km)
+        {
+            if (CurrentFuel <= km * FuelFor1Km)
+            {
+                Console.WriteLine("You dont have enough fuel");          
+                
+            }
+            else
+            {
+                CurrentFuel = CurrentFuel - km * FuelFor1Km;
+                Millage = Millage + km;
+                Console.WriteLine($"brand - {Brand}  model - {Model}  currentFuel - {CurrentFuel}  fuelper1km - {FuelFor1Km}  millage - {Millage}");
+            }
+        }
+        //public void FullData()
+        //{
+        //    Console.WriteLine($"brand - {Brand}  model - {Model}  currentFuel - {CurrentFuel}  fuelper1km - {FuelFor1Km}  millage - {Millage}");
+        //}
+        
+
+    }
+}
---
+++ Program class.cs
@@ -0,0 +1,18 @@
+﻿namespace resize___class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Brand = "BMW";
+            car.Model = "07";
+            car.CurrentFuel = 30;
+            car.FuelFor1Km = 4;
+            car.Millage = 500;
+            car.Drive(8);
+            //car.FullData();
+
+        }
+    }
+}
